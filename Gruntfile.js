806aa3f6513cbc0d472882f5d8ff8d2b607d1b08
Dave <callremz@gmail.com>
2020-09-12 12:41:29 +0100

    chore: install deps and setup grunt

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+var matchDep = require('matchdep');
+
+module.exports = function (grunt) {
+  matchDep.filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    terser: {
+      build: {
+        options: {
+          parse: {
+            ecma: 5
+          }
+        },
+        files: {
+          'build/index.js': ['./js/index.js']
+        }
+      }
+    },
+    uncss: {
+      build: {
+        options: {
+          report: 'min'
+        },
+        files: {
+          'build/styles.css': ['./index.html']
+        }
+      }
+    },
+    cssmin: {
+      build: {
+        files: [
+          { src: 'build/styles.css', dest: 'build/styles.css' }
+        ]
+      }
+    },
+    inline: {
+      dist: {
+        src: 'build/index.html',
+        dest: 'build/index.html'
+      }
+    },
+    processhtml: {
+      options: {
+        data: {
+          message: 'Hello world!'
+        }
+      },
+      dist: {
+        files: {
+          'build/index.html': ['./index.html']
+        }
+      }
+    },
+    copy: {
+      target: {
+        files: [
+          {expand: true, src: ['images/**'], dest: 'build/images'}
+        ]
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['terser', 'uncss', 'cssmin', 'processhtml', 'inline', 'copy']);
+};
